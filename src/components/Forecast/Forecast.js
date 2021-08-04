7dc66babb489812d9d698df5b73287b429452163
RonaldoMaia <ronaldo@thinktankautomation.ca>
2021-08-04 15:19:15 -0700

    Implemented changes

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,122 +1,126 @@
-
-import classes from './Forecast.module.css'; 
-import React, { useState } from 'react';
-import Conditions from '../Conditions/Conditions';
+import classes from "./Forecast.module.css";
+import React, { useState } from "react";
+import Conditions from "../Conditions/Conditions";
 
 const Forecast = () => {
+	let [city, setCity] = useState("");
+	let [country, setCountry] = useState("");
+	let [unit, setUnit] = useState("imperial");
+	let [responseObj, setResponseObj] = useState({});
+	let [error, setError] = useState(false);
+	let [loading, setLoading] = useState(false);
+	// eslint-disable-next-line no-unused-vars
+	const uriEncodedCity = encodeURIComponent(city);
 
-    let [city, setCity] = useState('');
-    let [country, setCountry] = useState('');
-    let [unit, setUnit] = useState('imperial');
-    let [responseObj, setResponseObj] = useState({});
-    let [error, setError] = useState(false);
-    let [loading, setLoading] = useState(false);
-    // eslint-disable-next-line no-unused-vars
-    const uriEncodedCity = encodeURIComponent(city);
-    
-   function getForecast(e) {
-       e.preventDefault();
-       if (city.length === 0) {
-        return setError(true);
-    }
-    e.preventDefault();
-    if (country.length === 0){
-        return setError(true);
-    }
+	function getForecast(e) {
+		e.preventDefault();
+		if (city.length === 0) {
+			return setError(true);
+		}
+		e.preventDefault();
+		if (country.length === 0) {
+			return setError(true);
+		}
 
-    setError(false);
-   setResponseObj({});
-  
-   setLoading(true);
-  
-   let uriEncodedCity = encodeURIComponent(city);
-   let uriEncodeCountry = encodeURIComponent(country);
+		setError(false);
+		setResponseObj({});
 
-    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity},${uriEncodeCountry}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "9c82fb2f60msh660633c2ea954dcp1fb8a0jsn5c0936bb6a61",
-            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-        }
-    })
- 
-       .then(response => response.json())
-       .then(response => {
-           setResponseObj(response)
-           if (response.cod !== 200) {
-            throw new Error()
-        }
-        setResponseObj(response);
-       setLoading(false);
-       })
+		setLoading(true);
 
-    .catch(err => {
-        setError(true);
-       setLoading(false);
-       console.log(err.message);
-    });
-   }
+		let uriEncodedCity = encodeURIComponent(city);
+		let uriEncodeCountry = encodeURIComponent(country);
 
-   return (
-    <div>
-    <h2>Find Current Weather Conditions</h2>
-    <form onSubmit={getForecast}>
-    <div class="card" >
-                <input className={classes.TextInput}
-                    type="text"
-                    placeholder="Enter Country"
-                    maxLength="50"
-                    value={country}
-                    onChange={(e) => setCountry(e.target.value)}
-                    />
-                <input className={classes.TextInput}
-                    type="text"
-                    placeholder="Enter City"
-                    maxLength="50"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                    />
-                <button className={classes.Button} type="button submit">Get Forecast</button>
-   
-                <label className={classes.Radio}>
-                    <input
-                        type="radio"
-                        name="units"
-                        checked={unit === "imperial"}
-                        value="imperial"
-                        onChange={(e) => setUnit(e.target.value)}         
-                        />
-                    Fahrenheit
-                </label>
+		fetch(
+			`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity},${uriEncodeCountry}`,
+			{
+				method: "GET",
+				headers: {
+					"x-rapidapi-key":
+						"9c82fb2f60msh660633c2ea954dcp1fb8a0jsn5c0936bb6a61",
+					"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+				},
+			}
+		)
+			.then((response) => response.json())
+			.then((response) => {
+				setResponseObj(response);
+				if (response.cod !== 200) {
+					throw new Error();
+				}
+				setResponseObj(response);
+				setLoading(false);
+			})
 
-                <label className={classes.Radio}>
-                    <input
-                        type="radio"
-                        name="units"
-                        checked={unit === "metric"}
-                        value="metric"
-                        onChange={(e) => setUnit(e.target.value)}  
-                        />
-                    Celcius
-                </label>  
-                </div>                        
-            </form>
+			.catch((err) => {
+				setError(true);
+				setLoading(false);
+				console.log(err.message);
+			});
+	}
 
-            <Conditions
-              responseObj={responseObj}
-              error={error} //new
-              loading={loading} //new
-              />
-             
-    </div>
-    /* <div>
+	return (
+		<div>
+			<h2>Find Current Weather Conditions</h2>
+			<form onSubmit={getForecast}>
+				<div className="card">
+					<input
+						className={classes.TextInput}
+						type="text"
+						placeholder="Enter Country"
+						maxLength="50"
+						value={country}
+						onChange={(e) => setCountry(e.target.value)}
+					/>
+					<input
+						className={classes.TextInput}
+						type="text"
+						placeholder="Enter City"
+						maxLength="50"
+						value={city}
+						onChange={(e) => setCity(e.target.value)}
+					/>
+					<button className={classes.Button} type="button submit">
+						Get Forecast
+					</button>
+
+					<label className={classes.Radio}>
+						<input
+							type="radio"
+							name="units"
+							checked={unit === "imperial"}
+							value="imperial"
+							onChange={(e) => setUnit(e.target.value)}
+						/>
+						Fahrenheit
+					</label>
+
+					<label className={classes.Radio}>
+						<input
+							type="radio"
+							name="units"
+							checked={unit === "metric"}
+							value="metric"
+							onChange={(e) => setUnit(e.target.value)}
+						/>
+						Celcius
+					</label>
+				</div>
+			</form>
+
+			<Conditions
+				responseObj={responseObj}
+				error={error} //new
+				loading={loading} //new
+			/>
+		</div>
+		/* <div>
         <h2>Find Current Weather Conditions</h2>
         <div>
             {JSON.stringify(responseObj)}
         </div>
         <button onClick={getForecast}>Get Forecast</button>
     </div> */
-   )
-}
+	);
+};
 
 export default Forecast;
--- src/components/ForecastBonus/ForecastBonus.js
+++ src/components/ForecastBonus/ForecastBonus.js
@@ -1,135 +1,168 @@
-
-import classes from './ForecastBonus.module.css'; 
-import React, { useState } from 'react';
-import Conditions from '../Conditions/Conditions';
-
-
+import classes from "./ForecastBonus.module.css";
+import React, { useEffect, useState } from "react";
+import Conditions from "../Conditions/Conditions";
 
 const Forecast = () => {
+	// Data that you want to use in the form
+	const uriInitial = {
+		city: "",
+		country: "",
+		unit: "imperial",
+	};
+	const initialPlace = {
+		city: "",
+		country: "",
+	};
 
-    let [city, setCity] = useState('');
-    let [country, setCountry] = useState('');
-    let [unit, setUnit] = useState('imperial');
-    let [responseObj, setResponseObj] = useState({});
-    let [error, setError] = useState(false);
-    let [loading, setLoading] = useState(false);
-    // eslint-disable-next-line no-unused-vars
-    const uriEncodedCity = encodeURIComponent(city);
-    
-   function getForecast(e) {
-       e.preventDefault();
-       if (city.length === 0) {
-        return setError(true);
-    }
-    e.preventDefault();
-    if (country.length === 0){
-        return setError(true);
-    }
+	const [uri, setUri] = useState(uriInitial);
+	const [place, setPlace] = useState(initialPlace);
+	const [unit, setUnit] = useState("imperial");
+	const [message, setMessage] = useState("");
+	const [responseObj, setResponseObj] = useState(null);
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	// eslint-disable-next-line no-unused-vars
 
-    setError(false);
-   setResponseObj({});
-  
-   setLoading(true);
-  
-   let uriEncodedCity = encodeURIComponent(city);
-   let uriEncodeCountry = encodeURIComponent(country);
+	// In this function you deal with the fields in the form
+	const handleChange = (e) => {
+		setError(false);
+		const { name, value } = e.target;
+		setPlace({ ...place, [name]: value });
+	};
 
-    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity},${uriEncodeCountry}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "9c82fb2f60msh660633c2ea954dcp1fb8a0jsn5c0936bb6a61",
-            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-        }
-    })
- 
-       .then(response => response.json())
-       .then(response => {
-           setResponseObj(response)
-           if (response.cod !== 200) {
-            throw new Error()
-        }
-        setResponseObj(response);
-       setLoading(false);
-       })
+	useEffect(() => {
+		if (place.city !== "" && place.country !== "") {
+			setUri({
+				...uri,
+				city: encodeURIComponent(place.city),
+				country: encodeURIComponent(place.country),
+				unit: unit,
+			});
+		}
+	}, [place, unit]); // eslint-disable-line
 
-    .catch(err => {
-        setError(true);
-       setLoading(false);
-       console.log(err.message);
-    });
-   }
-   return (
-<div class="container">
-    <div class="card">
-        <div class="row">
-            <div id="container" class="col">
-            <img class="card-img" src="https://cdn.dribbble.com/users/925716/screenshots/3333720/attachments/722375/night.png" alt="night"></img>
-            <div class="card-body">
-                <h2 id="text">Weather Finder</h2>
-                <p id="text2">Temperature, conditions and more</p>
-                </div>
-                </div>
-            <div id="container" class="col">
-                <form onSubmit={getForecast}> 
-                    <div class="row"> 
-                        <input className={classes.TextInput}
-                            type="text"
-                            placeholder="City"
-                            maxLength="50"
-                            value={city}
-                            onChange={(e) => setCity(e.target.value)}
-                            />            
-                        <input className={classes.TextInput}
-                            type="text"
-                            placeholder="Country"
-                            maxLength="50"
-                            value={country}
-                            onChange={(e) => setCountry(e.target.value)}
-                            />                       
-                        <button className={classes.Button} type="button submit">Get Forecast</button>
-                    </div>
-                    <div class="row">
-                        <label className={classes.Radio}>
-                            <input
-                                type="radio"
-                                name="units"
-                                checked={unit === "imperial"}
-                                value="imperial"
-                                onChange={(e) => setUnit(e.target.value)}
-                            />
-                                Fahrenheit
-                        </label>
+	const fetchData = async (unit, city, country) => {
+		fetch(
+			`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${city},${country}`,
+			{
+				method: "GET",
+				headers: {
+					"x-rapidapi-key":
+						"9c82fb2f60msh660633c2ea954dcp1fb8a0jsn5c0936bb6a61",
+					"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+				},
+			}
+		)
+			.then((response) => response.json())
+			.then((response) => {
+				setResponseObj(response);
+				if (response.cod !== 200) {
+					throw new Error();
+				}
+				setResponseObj(response);
+				setLoading(false);
+			})
 
-                        <label className={classes.Radio}>
-                            <input
-                                type="radio"
-                                name="units"
-                                checked={unit === "metric"}
-                                value="metric"
-                                onChange={(e) => setUnit(e.target.value)}  
-                                />
-                                Celcius
-                        </label>    
-                        </div>         
-                
-                        <Conditions
-                        responseObj={responseObj}
-                        error={error} //new
-                        loading={loading} //new
-                        />
-                </form>
-              </div>
-        </div>
-    </div>
-</div>
-    /* <div>
+			.catch((err) => {
+				setError(true);
+				setLoading(false);
+				console.log(err.message);
+			});
+	};
+
+	const getForecast = (e) => {
+		e.preventDefault();
+		if (place.city === "" || place.country === "") {
+			setError(true);
+			setMessage("Please, enter a valid city and country");
+		} else {
+			setLoading(true);
+			fetchData(uri.unit, uri.city, uri.country);
+		}
+	};
+
+	return (
+		<div class="container">
+			<div class="card">
+				<div class="row">
+					<div id="container" class="col">
+						<img
+							class="card-img"
+							src="https://cdn.dribbble.com/users/925716/screenshots/3333720/attachments/722375/night.png"
+							alt="night"
+						></img>
+						<div class="card-body">
+							<h2 id="text">Weather Finder</h2>
+							<p id="text2">Temperature, conditions and more</p>
+						</div>
+					</div>
+					<div id="container" className="col">
+						<form onSubmit={getForecast}>
+							<div className="row">
+								<input
+									className={classes.TextInput}
+									type="text"
+									placeholder="City..."
+									maxLength="100"
+									name="city"
+									value={place.city}
+									onChange={handleChange}
+								/>
+								<input
+									className={classes.TextInput}
+									type="text"
+									name="country"
+									placeholder="Country..."
+									maxLength="100"
+									value={place.country}
+									onChange={handleChange}
+								/>
+								<button className={classes.Button} type="button submit">
+									Get Forecast
+								</button>
+							</div>
+							<div className="row">
+								<label className={classes.Radio}>
+									<input
+										type="radio"
+										name="units"
+										value="imperial"
+										checked={unit === "imperial"}
+										onChange={(e) => setUnit(e.target.value)}
+									/>
+									Fahrenheit
+								</label>
+
+								<label className={classes.Radio}>
+									<input
+										type="radio"
+										name="units"
+										checked={unit === "metric"}
+										value="metric"
+										onChange={(e) => setUnit(e.target.value)}
+									/>
+									Celcius
+								</label>
+							</div>
+							{error && <small style={{ color: "red" }}>{message}</small>}
+							{!error && responseObj !== null && responseObj !== undefined && (
+								<>
+									<Conditions responseObj={responseObj} loading={loading} />
+								</>
+							)}
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		/* <div>
         <h2>Find Current Weather Conditions</h2>
         <div>
             {JSON.stringify(responseObj)}
         </div>
         <button onClick={getForecast}>Get Forecast</button>
     </div> */
-   )
-}
+	);
+};
 
 export default Forecast;
